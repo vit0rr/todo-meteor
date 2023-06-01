0d43cacaad427c53ed7485157451370cbe659659
vitor <vi.souza.almeida@protonmail.com>
2023-05-31 22:12:02 -0300

    feat(tasks): create new task form (#3)

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useTracker } from "meteor/react-meteor-data";
 import { TasksCollection } from "../api/TasksCollection";
 import { Task } from "./Task.jsx";
+import { TaskForm } from "./TaskForm";
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm/>
       <ul>
         {tasks.map((task) => (
           <Task key={task._id} task={task} />
