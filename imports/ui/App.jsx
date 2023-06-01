237e913bf833de4cbf5be8665cbc1b13d772cda1
Vitor <vi.souza.almeida@protonmail.com>
2023-05-31 21:46:27 -0300

    feat(collection): create the tasks collection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { TasksCollection } from "../api/TasksCollection";
 import { Task } from "./Task.jsx";
 
-const tasks = [
-  { _id: 1, text: "First Task" },
-  { _id: 2, text: "Second Task" },
-  { _id: 3, text: "Third Task" },
-];
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <ul>
-      {tasks.map((task) => (
-        <Task key={task._id} task={task} />
-      ))}
-    </ul>
-  </div>
-);
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ))}
+      </ul>
+    </div>
+  );
+};
