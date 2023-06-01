099effaed8d3295cd3eb232e7a3984a20e905488
vitor <vi.souza.almeida@protonmail.com>
2023-05-31 22:30:58 -0300

    feat(tasks): delete tasks button (#4)

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,17 +4,34 @@ import { TasksCollection } from "../api/TasksCollection";
 import { Task } from "./Task.jsx";
 import { TaskForm } from "./TaskForm";
 
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked,
+    },
+  });
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  const tasks = useTracker(() =>
+    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+  );
 
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
 
-      <TaskForm/>
+      <TaskForm />
       <ul>
         {tasks.map((task) => (
-          <Task key={task._id} task={task} />
+          <Task
+            key={task._id}
+            task={task}
+            onCheckboxClick={toggleChecked}
+            onDeleteClick={deleteTask}
+          />
         ))}
       </ul>
     </div>
